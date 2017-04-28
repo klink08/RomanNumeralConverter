4db57be47dd513a8f28aafa2ebb634b446a7f5db
klink08 <klink08@github.com>
2017-04-27 22:33:54 -0400

    Refactored the splices to remove more than one element at a time if applicable. Changed the pattern matter to a slice. Might consider regex in the future.

--- public/javascripts/romanNumeralConverter.js
+++ public/javascripts/romanNumeralConverter.js
@@ -1,39 +1,26 @@
 function arabicToRomanNumeral(arabicNumber){
     var romanNumeralArray = [];
     for(var i = 1; i <= arabicNumber; i++) {
+        var initialArrayLength = romanNumeralArray.length;
         if(i % 50 === 0 && i % 100 !== 0){
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
+            romanNumeralArray.splice(initialArrayLength - 2, 2);
             romanNumeralArray.splice(romanNumeralArray.length - 2, 1);
-        } else if(romanNumeralArray[romanNumeralArray.length - 5] === 'X' &&
-            romanNumeralArray[romanNumeralArray.length - 4] === 'X' &&
-            romanNumeralArray[romanNumeralArray.length - 3] === 'X' &&
-            romanNumeralArray[romanNumeralArray.length - 2] === 'I' &&
-            romanNumeralArray[romanNumeralArray.length - 1] === 'X'){
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
+        } else if(romanNumeralArray.slice(initialArrayLength - 5, initialArrayLength).toString().replace(/,/g , "") === "XXXIX"){
+            romanNumeralArray.splice(initialArrayLength - 3, 3);
             romanNumeralArray.splice(romanNumeralArray.length - 2, 1);
             romanNumeralArray.push('L');
         } else if(i % 10 === 0){
-            romanNumeralArray.splice(romanNumeralArray.length - 2, 1);
+            romanNumeralArray.splice(initialArrayLength - 2, 1);
         } else if(i % 10 === 9){
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
+            romanNumeralArray.splice(initialArrayLength - 2, 2);
             romanNumeralArray.splice(romanNumeralArray.length - 2, 1);
             romanNumeralArray.push('X');
         } else if(i % 5 === 0 && i % 10 !== 0){
-            romanNumeralArray.splice(romanNumeralArray.length - 2, 1);
-        } else if(romanNumeralArray[romanNumeralArray.length - 1] === 'I' &&
-                  romanNumeralArray[romanNumeralArray.length - 2] === 'I' &&
-                  romanNumeralArray[romanNumeralArray.length - 3] === 'I') {
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
-            romanNumeralArray.splice(romanNumeralArray.length - 1, 1);
+            romanNumeralArray.splice(initialArrayLength - 2, 1);
+        } else if(romanNumeralArray.slice(initialArrayLength - 3, initialArrayLength).toString().replace(/,/g , "") === "III") {
+            romanNumeralArray.splice(initialArrayLength - 2, 2);
             romanNumeralArray.push('V');
-        } else if(romanNumeralArray[romanNumeralArray.length - 1] !== 'I' ||
-            romanNumeralArray[romanNumeralArray.length - 2] !== 'I' ||
-            romanNumeralArray[romanNumeralArray.length - 3] !== 'I' ||
-            i <= 3) {
+        } else {
             romanNumeralArray.push('I');
         }
     }
