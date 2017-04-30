5bd9ad32f34464b7b8b88d86e0d1958339ef9077
klink08 <klink08@github.com>
2017-04-30 16:12:58 -0400

    Adding in final values for roman symbols and adding final jasmine tests outlined in the acceptance criteria.

--- public/javascripts/romanNumeralConverter.js
+++ public/javascripts/romanNumeralConverter.js
@@ -33,7 +33,7 @@
 // }
 
 function arabicToRomanNumeral(num) {
-    var romanSymbols = {C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
+    var romanSymbols = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
         romanConversion = '';
     for (var i in romanSymbols ) {
         while ( num >= romanSymbols[i] ) {
--- public/tests/romanNumeralConverter.spec.js
+++ public/tests/romanNumeralConverter.spec.js
@@ -11,19 +11,19 @@ describe('romanNumeralConverter',function() {
         it('9 to IX', function(){
             expect(arabicToRomanNumeral(9)).toBe('IX');
         });
+
+        it('1066 to MLXVI', function(){
+            expect(arabicToRomanNumeral(1066)).toBe('MLXVI');
+        });
+
+        it('1989 to MCMLXXXIX', function(){
+            expect(arabicToRomanNumeral(1989)).toBe('MCMLXXXIX');
+        });
     });
 
     describe('should convert the first 3 arabic numbers to Is: ', function() {
-        it('1 to I', function() {
-            expect(arabicToRomanNumeral(1)).toBe('I');
-        });
-
         it('2 to II', function() {
             expect(arabicToRomanNumeral(2)).toBe('II');
-        });
-
-        it('3 to III', function() {
-            expect(arabicToRomanNumeral(3)).toBe('III');
         });
     });
 
